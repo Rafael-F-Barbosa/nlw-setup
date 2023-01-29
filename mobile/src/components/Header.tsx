4bafe32859d61adbbdfc9bb39413fa90fdfb5223
Rafael Barbosa <rafaelfbarbosa7@gmail.com>
2023-01-28 21:11:27 -0300

    💄 mobile routing and screens

--- mobile/src/components/Header.tsx
+++ mobile/src/components/Header.tsx
@@ -1,29 +1,32 @@
-import {View, TouchableOpacity, Text} from "react-native"
-import {Feather} from '@expo/vector-icons'
-import colors from "tailwindcss/colors"
+import { View, TouchableOpacity, Text } from "react-native";
+import { Feather } from '@expo/vector-icons';
+import colors from 'tailwindcss/colors';
+import { useNavigation } from "@react-navigation/native";
 
-import Logo from "../assets/logo.svg"
+import Logo from '../assets/logo.svg';
 
-export function Header(){
-    return (
-        <View className="w-full flex-row items-center justify-between">
-            <Logo/>
-            <TouchableOpacity
-                activeOpacity={0.7}
-                className="flex-row h-11 px-4 border border-violet-500 rounder-lg items-center"
-            >
+export function Header() {
+  const { navigate } = useNavigation()
 
-                <Feather 
-                    name="plus"
-                    color={colors.violet[500]}
-                    size={20}
-                />
-                <Text className="text-white ml-3 font-semibold text-base">
-                    Novo
-                </Text>
+  return (
+    <View className="w-full flex-row items-center justify-between">
+      <Logo />
 
-            </TouchableOpacity>
-        </View>
+      <TouchableOpacity 
+        activeOpacity={0.7}
+        className="flex-row h-11 px-4 border border-violet-500 rounded-lg items-center" 
+        onPress={() => navigate('new')} 
+      >
+        <Feather 
+          name="plus"
+          color={colors.violet[500]}
+          size={20}
+        />
 
-    )
+        <Text className="text-white ml-3 font-semibold text-base">
+          Novo
+        </Text>
+      </TouchableOpacity>
+    </View>
+  )
 }
